4a255c8c9ef7397fda0b1e9e254fbf42892e6569
Daisuke Fujita <dtanshi45@gmail.com>
2015-12-16 16:17:36 +0900

    Remove needless test case

--- spec/aws4_signer_spec.cr
+++ spec/aws4_signer_spec.cr
@@ -11,31 +11,20 @@ describe Aws4Signer do
   end
 
   describe "sign" do
-    describe "without uri" do
-      let(:uri) { nil }
+    let(:uri) { URI.parse("https://example.org/foo/bar?baz=blah") }
 
-      it "should raise ArgumentError" do
-        ex = assert_raises { signer.sign("put", uri, headers, "hello") }
-        assert_equal "URI must be provided", ex.message
-      end
-    end
+    it "should return Signature" do
+      signature = signer.sign("PUT", uri, headers, "hello")
 
-    describe "with uri" do
-      let(:uri) { URI.parse("https://example.org/foo/bar?baz=blah") }
-
-      it "should return Signature" do
-        signature = signer.sign("PUT", uri, headers, "hello")
-
-        assert signature.is_a?(Aws4Signer::Signature)
-        assert_equal "AKID", signature.access_key_id
-        assert_equal "SECRET", signature.secret_access_key
-        assert_equal "xx-region-1", signature.region
-        assert_equal "svc", signature.service
-        assert_equal "PUT", signature.verb
-        assert_equal uri, signature.uri
-        assert_equal "hello", signature.body
-        assert_equal "bar", signature.headers["foo"]
-      end
+      assert signature.is_a?(Aws4Signer::Signature)
+      assert_equal "AKID", signature.access_key_id
+      assert_equal "SECRET", signature.secret_access_key
+      assert_equal "xx-region-1", signature.region
+      assert_equal "svc", signature.service
+      assert_equal "PUT", signature.verb
+      assert_equal uri, signature.uri
+      assert_equal "hello", signature.body
+      assert_equal "bar", signature.headers["foo"]
     end
   end
 
